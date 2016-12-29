7c02a5d16bcadafb6c6bcaf313842b3ec3196a4e
Abdul Matin <matinict@gmail.com>
2016-12-29 23:17:34 +0600

    circleArea

---
+++ circleArea.py
@@ -0,0 +1,10 @@
+def circleArea():
+	import math
+	r=float(input("Please Enter A Radius: "))
+	cArea= math.pi*pow(r,2)
+	print("The Circlr Area Is :",cArea)
+
+	
+##>>> circleArea()
+##Please Enter A Radius: 4
+##The Circlr Area Is : 50.26548245743669

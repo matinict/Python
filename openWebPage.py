f9ed2657b37492362f9cba428799f571fea10fb4
Abdul Matin <matinict@gmail.com>
2016-12-30 00:23:17 +0600

    openWebPage

---
+++ openWebPage.py
@@ -0,0 +1,11 @@
+def openWebPage():
+	import webbrowser
+	url = input("Enter URL(Ex.http://bdnews24.com): ")
+	webbrowser.open(url)
+	print("You are visiting " + url)
+
+	
+##>>> openWebPage()
+##Enter URL(Ex.http://bdnews24.com): http://bangla.bdnews24.com
+##You are visiting http://bangla.bdnews24.com
+ 

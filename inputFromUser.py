0197970691b24281c0f3fa447d289b70bab7dfcd
Abdul Matin <matinict@gmail.com>
2016-12-29 22:04:22 +0600

    inputFromUser

---
+++ inputFromUser.py
@@ -0,0 +1,11 @@
+def inputFromUser():
+	name = input("Please Enter Your Name: ")
+	age = input("Please Enter Your Age: ")
+	print("Your Name is "+name+" & Age " + age)
+
+	
+##>>> inputFromUser()
+##Please Enter Your Name: Matin Rahman
+##Please Enter Your Age: 29
+##Your Name is Matin Rahman & Age 29
+##>>> 
